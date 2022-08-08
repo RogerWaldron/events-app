c1cedcb201f5fb4ac6183f3a873182540dfa6b82
Roger Waldron <roger.waldron@gmail.com>
2022-08-09 00:02:24 +0200

    fix: :bug: Added single root element needed for JSX

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,8 @@
 export default function AboutPage() {
   return (
-    <h1>About Us</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus fugiat quo possimus illo sed. Autem, aliquid exercitationem sunt obcaecati quae cum odio quidem cumque corporis deleniti sequi nemo asperiores aspernatur!</p>
+    <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus fugiat quo possimus illo sed. Autem, aliquid exercitationem sunt obcaecati quae cum odio quidem cumque corporis deleniti sequi nemo asperiores aspernatur!</p>
+    </div>
   )
 }
