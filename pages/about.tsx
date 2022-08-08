53383ab96334169af1d5f7bfbdd513024ab2f6c1
Roger Waldron <roger.waldron@gmail.com>
2022-08-08 23:48:42 +0200

    feat: Added about page

---
+++ pages/about.tsx
@@ -0,0 +1,6 @@
+export default function AboutPage() {
+  return (
+    <h1>About Us</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus fugiat quo possimus illo sed. Autem, aliquid exercitationem sunt obcaecati quae cum odio quidem cumque corporis deleniti sequi nemo asperiores aspernatur!</p>
+  )
+}
